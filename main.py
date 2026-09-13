b6df2ccc08dc3ad9f28a2a5ce0ca6a5e36a56839
samar abu alouf <samarabualouf42@gmail.com>
2026-09-13 17:58:03 +0300

    Add more student information

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-name = "Your Name" 
-major = "Your Major" 
-university = "Your University" 
+name = "samar abo alouf " 
+major = "AI" 
+university = "UCAS" 
+hobby = "Reading" 
+career_goal = "AI Engineer" 
+second_programming_language = "Python"
 print("Student Profile") 
 print("----------------") 
 print("Name:", name) 
 print("Major:", major) 
 print("University:", university) 
+print("Hobby:", hobby) 
+print("Career Goal:", career_goal) 
+print("Second Programming Language:", second_programming_language)
