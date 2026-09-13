3e41a387688f95bbad2e9e21a159fcc0aada338d
samar abu alouf <samarabualouf42@gmail.com>
2026-09-13 17:44:27 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Your Name" 
+major = "Your Major" 
+university = "Your University" 
+print("Student Profile") 
+print("----------------") 
+print("Name:", name) 
+print("Major:", major) 
+print("University:", university) 
